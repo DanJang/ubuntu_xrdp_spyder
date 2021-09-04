ce6a6d296ec702d010964d939967f433f8517c2b
Daniel Jang <danieljang2010@gmail.com>
2021-09-04 18:34:40 +0800

    update

--- rdp_launcher.bat
+++ rdp_launcher.bat
@@ -4,11 +4,10 @@
 # Alternatively, below single line will do the similar though it only sets width and height
 # start "ubuntu20_xrdp" "%windir%\system32\mstsc.exe" /v:localhost:3390 /w:1800 /h:900
 
-# create a temporary rdp file
-(echo full address:s:localhost:3390& echo username:s:ubuntu& echo screen mode id:i:1& echo desktopwidth:i:1800& echo desktopheight:i:900& echo winposstr:s:0,1,0,0,1800,900& echo authentication level:i:0& echo redirectprinters:i:0 ) > temp.rdp
-#     use below for entire screen in 2nd monitor
-#(echo full address:s:localhost:3390& echo username:s:ubuntu& echo screen mode id:i:2& echo desktopwidth:i:1800& echo desktopheight:i:900& echo winposstr:s:0,1,44,-1748,574,-1317& echo authentication level:i:0& echo redirectprinters:i:0 ) > temp.rdp
-
+# create a temporary rdp file. (use 'rem' to commentify)
+rem (echo full address:s:localhost:3390& echo username:s:ubuntu& echo desktopwidth:i:1800& echo desktopheight:i:900& echo screen mode id:i:1& echo winposstr:s:0,1,0,0,1800,900& echo authentication level:i:0& echo redirectprinters:i:0 ) > temp.rdp
+#    use below for entire screen in 2nd monitor 
+(echo full address:s:localhost:3390& echo username:s:ubuntu& echo desktopwidth:i:1800& echo desktopheight:i:900& echo screen mode id:i:2& echo winposstr:s:0,1,44,-1748,574,-1317& echo authentication level:i:0& echo redirectprinters:i:0 ) > temp.rdp
 
 # run mstsc.exe with the rdp file
 start "ubuntu20_xrdp" "%windir%\system32\mstsc.exe" temp.rdp
