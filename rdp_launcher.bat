c0dfc9622749f978d8027e0b04649130afdddbaf
Daniel Jang <danieljang2010@gmail.com>
2021-09-04 15:20:48 +0800

    Update rdp_launcher.bat
    adding example for full screen on 2nd monitor

--- rdp_launcher.bat
+++ rdp_launcher.bat
@@ -1,11 +1,14 @@
-# A batch file to conveniently launch a Remote Desktop Connection app with custom settings and automatically close itself
-# Custom settings : remote desktop window size, login user name, suppressing some Remote Desktop popups
+# A batch file to conveniently launch a Remote Desktop with custom settings and automatically close the CMD window
+# Custom settings : RDP Server URL, Remote Desktop window size, login user name, suppressing some Remote Desktop popups
 #
-# alternatively, below single line will do the similar though it only set width and height
+# Alternatively, below single line will do the similar though it only sets width and height
 # start "ubuntu20_xrdp" "%windir%\system32\mstsc.exe" /v:localhost:3390 /w:1800 /h:900
 
 # create a temporary rdp file
 (echo full address:s:localhost:3390& echo username:s:ubuntu& echo screen mode id:i:1& echo desktopwidth:i:1800& echo desktopheight:i:900& echo winposstr:s:0,1,0,0,1800,900& echo authentication level:i:0& echo redirectprinters:i:0 ) > temp.rdp
+#     use below for entire screen in 2nd monitor
+#(echo full address:s:localhost:3390& echo username:s:ubuntu& echo screen mode id:i:2& echo desktopwidth:i:1800& echo desktopheight:i:900& echo winposstr:s:0,1,44,-1748,574,-1317& echo authentication level:i:0& echo redirectprinters:i:0 ) > temp.rdp
+
 
 # run mstsc.exe with the rdp file
 start "ubuntu20_xrdp" "%windir%\system32\mstsc.exe" temp.rdp
